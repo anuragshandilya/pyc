09b3e6e8c06d994129a80a840f3380240f086c5e
sherpya <sherpya@netfarm.it>
2008-04-25 23:18:26 +0000

    fixed mingw compilation

--- pyc.c
+++ pyc.c
@@ -53,6 +53,8 @@ typedef unsigned __int16 uint16_t;
 typedef signed   __int16 int16_t;
 typedef unsigned __int8  uint8_t;
 typedef signed   __int8  int8_t;
+#else
+#include <inttypes.h>
 #endif
 
 #ifndef MAX_PATH
