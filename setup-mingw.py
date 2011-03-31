586c98fcf4d6eb6366ebf6d26306a734cbdcf132
sherpya <sherpya@netfarm.it>
2011-03-31 14:16:00 +0200

    gitignore and missing stuff

---
+++ setup-mingw.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+from distutils.core import setup, Extension
+from sys import platform
+from os import environ
+
+CFLAGS = [ '-Wall', '-O2', '-fno-strict-aliasing' ]
+LDFLAGS = [ '-L.' ]
+LIBS = [ 'clamav' ]
+CLINCLUDE = [ '.' ]
+CLLIB = []
+
+pyc = Extension('pyc',
+                sources = ['pyc.c'],
+                libraries = LIBS,
+                extra_objects = CLLIB,
+                extra_link_args = LDFLAGS,
+                extra_compile_args = CFLAGS)
+
+# Build : python setup.py build
+# Install : python setup.py install
+# Register : python setup.py register
+
+setup (name = 'pyc',
+       version = '2.0.95',
+       author = 'Gianluigi Tiesi',
+       author_email = 'sherpya@netfarm.it',
+       license ='GPL',
+       keywords="python, clamav, antivirus, scanner, virus, libclamav",
+       include_dirs = CLINCLUDE,
+       ext_modules = [ pyc ])
